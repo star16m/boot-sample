39d436eaf5536ff646df74208f6a336288566c49
star16m <star16m@gmail.com>
2019-07-19 09:35:45 +0900

    change util classes from class to interface

--- core/src/main/java/star16m/bootsample/core/utils/SimpleUtil.java
+++ core/src/main/java/star16m/bootsample/core/utils/SimpleUtil.java
@@ -6,61 +6,60 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class SimpleUtil {
-    private SimpleUtil() {}
-    public static boolean isNull(Object object) {
+public interface SimpleUtil {
+    static boolean isNull(Object object) {
         return Objects.isNull(object);
     }
-    public static boolean isNotNull(Object object) {
+    static boolean isNotNull(Object object) {
         return !isNull(object);
     }
-    public static boolean isEmpty(String string) {
+    static boolean isEmpty(String string) {
         return isNull(string) || string.isEmpty();
     }
-    public static boolean isEmpty(Collection<?> collection) {
+    static boolean isEmpty(Collection<?> collection) {
         return isNull(collection) || collection.isEmpty();
     }
-    public static boolean isEmpty(Map<?,?> map) {
+    static boolean isEmpty(Map<?,?> map) {
         return isNull(map) || map.isEmpty();
     }
-    public static boolean isEmpty(Object[] objectArray) {
+    static boolean isEmpty(Object[] objectArray) {
         return isNull(objectArray) || objectArray.length == 0;
     }
-    public static <T> void must(T t, Predicate<T> predicate, String message) {
+    static <T> void must(T t, Predicate<T> predicate, String message) {
         Objects.requireNonNull(predicate);
         if (predicate.test(t)) {
             throw new SimpleException(message);
         }
     }
-    public static void mustNotNull(Object object) {
+    static void mustNotNull(Object object) {
         if (isNull(object)) {
             throw new SimpleException("Object must not be null");
         }
     }
 
-    public static void mustNull(Object object) {
+    static void mustNull(Object object) {
         if (!isNull(object)) {
             throw new SimpleException("Object must be null");
         }
     }
 
-    public static void mustEqual(Object a, Object b) {
+    static void mustEqual(Object a, Object b) {
         mustNotNull(a);
         mustNotNull(b);
         if (!a.equals(b)) {
             throw new SimpleException("Objects must be equals. a=[{}], b=[{}]", a, b);
         }
     }
-    public static String getString(String message) {
+    static String getString(String message) {
         return getString(message, "");
     }
-    public static String getString(String message, String defaultMessage) {
+    static String getString(String message, String defaultMessage) {
         if (isNull(message) || message.isEmpty()) {
             return defaultMessage;
         }
         return message;
     }
-    public static String getString(Map<String, String[]> map) {
+    static String getString(Map<String, String[]> map) {
         if (isNull(map) || map.isEmpty()) {
             return "";
         }
@@ -69,13 +68,13 @@ public class SimpleUtil {
                 .collect(Collectors.joining(", ", "{", "}"));
     }
 
-    public static String getString(String[] stringArray) {
+    static String getString(String[] stringArray) {
         if (isNull(stringArray) || isEmpty(stringArray)) {
             return "";
         }
         return Arrays.asList(stringArray).stream().collect(Collectors.joining(", "));
     }
-    public static boolean isNotNullAndIsNotEmpty(Map<String, Object> map) {
+    static boolean isNotNullAndIsNotEmpty(Map<String, Object> map) {
         return isNotNull(map) && !map.isEmpty();
     }
 }
--- core/src/main/java/star16m/bootsample/core/utils/WebUtil.java
+++ core/src/main/java/star16m/bootsample/core/utils/WebUtil.java
@@ -3,15 +3,13 @@ package star16m.bootsample.core.utils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class WebUtil {
+public interface WebUtil {
 
-    private WebUtil() {}
-
-    public static <T> ResponseEntity<T> response(T object) {
+    static <T> ResponseEntity<T> response(T object) {
         return response(object, HttpStatus.OK);
     }
 
-    public static <T> ResponseEntity<T> response(T object, HttpStatus httpStatus) {
+    static <T> ResponseEntity<T> response(T object, HttpStatus httpStatus) {
         return new ResponseEntity<>(object, httpStatus);
     }
 }
